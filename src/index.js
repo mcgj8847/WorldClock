41429b762ea7a029da80c439c1efd874a2bd1d7c
mcgj8847 <mcguzmanj@uoc.edu>
2025-06-24 21:29:43 -0600

    Updated Html and JS

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ function timeNow() {
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityhour = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#places");
